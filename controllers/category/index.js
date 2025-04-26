1cf66e48230ea5ea1b133d1cf3661f499a92a176
Hamza <hmza@gmail.com>
2025-04-26 16:47:11 +0500

    Add update category api

--- controllers/category/index.js
+++ controllers/category/index.js
@@ -6,5 +6,6 @@ const categoryService = require("../../services/category")
 router.post("/add-category/:id", categoryService.addCategory)
 router.get("/get-all-category/:id", categoryService.getAllCategory)
 router.get("/get-single-category/:id", categoryService.getSingleCategory)
+router.put("/update-category", categoryService.updateCategory)
 
 module.exports = router
--- services/category/index.js
+++ services/category/index.js
@@ -121,9 +121,42 @@ const getSingleCategory = asyncErrorHandler(async (req, res) => {
     });
 });
 
+// --------------- Update Category ------------------
+
+const updateCategory = asyncErrorHandler(async (req,res)=>{
+    const {category_id} = req.query
+    const {name} = req.body
+
+    if(!category_id){
+
+        return res.status(STATUS_CODES.NOT_FOUND).json({
+            statusCode:STATUS_CODES.NOT_FOUND,
+            message:TEXTS.ID_NOT_FOUND
+        })
+    }
+
+    const category = await Category.findOne({where:{
+        id:category_id
+    }})
+
+    if(!category){
+        return res.status(STATUS_CODES.NOT_FOUND).json({
+            statusCode:STATUS_CODES.NOT_FOUND,
+            message:TEXTS.NOT_FOUND
+        })
+    }
+
+    const updateCategory = await Category.update({name}, {
+        where:{id:category_id}
+    })
+
+    res.status(STATUS_CODES.SUCCESS).json({
+        statusCode:STATUS_CODES.SUCCESS,
+        message:TEXTS.UPDATED,
+        updateCategory
+    })
+
+})
 
 
-
-
-
-module.exports = {addCategory, getAllCategory, getSingleCategory}
+module.exports = {addCategory, getAllCategory, getSingleCategory, updateCategory}
